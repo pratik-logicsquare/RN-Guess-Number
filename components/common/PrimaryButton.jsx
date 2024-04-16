de1fd5d897aa68e550dad17b981c9d70a499779a
Pratik Sahu <pratiks@logic-square.com>
2024-04-16 10:47:19 +0530

    updated UI for startgame screen

--- components/common/PrimaryButton.jsx
+++ components/common/PrimaryButton.jsx
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const PrimaryButton = ({ title }) => {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed ? [styles.button, styles.pressed] : styles.button
+        }
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{title}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+  button: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,14 +1,62 @@
-import { View, Text, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import PrimaryButton from "../components/common/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
-      <PrimaryButton title="Reset" />
-      <PrimaryButton title="Confirm" />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton title="Reset" />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <PrimaryButton title="Confirm" />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    alignItems: "center",
+    gap: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#4e0329",
+    elevation: 4, // shadow for android
+    // shadow for iOS
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 50,
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    width: "50%",
+  },
+});
