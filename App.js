68015ef521d8ac1f49a6e709b1e4d76d9c160685
Pratik Sahu <pratiks@logic-square.com>
2024-04-16 18:55:11 +0530

    added support for custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { ImageBackground, SafeAreaView, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useFonts } from "expo-font";
 import { GameOverScreen, GameScreen, StartGameScreen } from "./screens";
 import Colors from "./constants/colors";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  // loading custom fonts
+  const [fontsLoaded, fontError] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
 
@@ -19,6 +35,10 @@ export default function App() {
 
   let screen = <StartGameScreen onPickNumber={_onPickNumber} />;
 
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   if (userNumber)
     screen = (
       <GameScreen userNumber={userNumber} onGameOver={_handleGameOver} />
@@ -30,6 +50,7 @@ export default function App() {
     <LinearGradient
       colors={[Colors.primary700, Colors.accent500]}
       style={styles.rootScreen}
+      onLayout={onLayoutRootView}
     >
       <ImageBackground
         source={require("./assets/images/background.png")}
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     color: Colors.accent500,
     marginVertical: 8,
-    fontWeight: "bold",
+    fontFamily: "open-sans-bold",
     width: 50,
     textAlign: "center",
   },
