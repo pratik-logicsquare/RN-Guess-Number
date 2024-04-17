ff20b1c54ba754e8bc5514479b09b18164afb1f3
Pratik Sahu <pratiks@logic-square.com>
2024-04-17 18:33:30 +0530

    added keyboard avoiding view in start game screen

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -4,6 +4,8 @@ import {
   StyleSheet,
   Alert,
   useWindowDimensions,
+  KeyboardAvoidingView,
+  ScrollView,
 } from "react-native";
 import { useState } from "react";
 import { Card, InstructionText, PrimaryButton, Title } from "../components/UI";
@@ -41,36 +43,43 @@ const StartGameScreen = ({ onPickNumber }) => {
   const marginTop = height < 380 ? 30 : 80;
 
   return (
-    <View style={[styles.rootContainer, { marginTop }]}>
-      <Title>Guess My Number</Title>
-      <Card style={styles.card}>
-        <InstructionText>Enter a number</InstructionText>
+    <ScrollView style={styles.screen}>
+      <KeyboardAvoidingView style={styles.screen} behavior="position">
+        <View style={[styles.rootContainer, { marginTop }]}>
+          <Title>Guess My Number</Title>
+          <Card style={styles.card}>
+            <InstructionText>Enter a number</InstructionText>
 
-        <TextInput
-          style={styles.chosenNumber}
-          maxLength={2}
-          keyboardType="number-pad"
-          value={enteredNumber}
-          onChangeText={_onChangeNumber}
-        />
+            <TextInput
+              style={styles.chosenNumber}
+              maxLength={2}
+              keyboardType="number-pad"
+              value={enteredNumber}
+              onChangeText={_onChangeNumber}
+            />
 
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={_resetNumber}>Reset</PrimaryButton>
-          </View>
+            <View style={styles.buttonsContainer}>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={_resetNumber}>Reset</PrimaryButton>
+              </View>
 
-          <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={_onConfirm}>Confirm</PrimaryButton>
-          </View>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={_onConfirm}>Confirm</PrimaryButton>
+              </View>
+            </View>
+          </Card>
         </View>
-      </Card>
-    </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     alignItems: "center",
   },
